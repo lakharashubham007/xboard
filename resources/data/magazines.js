c6435a22fa5fc15277297349c90f521c7f625063
lakharashubham007 <lakharashubham623@gmail.com>
2022-07-14 07:00:51 +0530

    CrioNewsFeed

---
+++ resources/data/magazines.js
@@ -0,0 +1,17 @@
+magazines = [
+  "https://flipboard.com/topic/indianpolitics.rss",
+  "https://flipboard.com/topic/indianstartups.rss",
+  "https://flipboard.com/topic/aajtak.rss"
+]
+
+/** Caution: Do not write your logic in this file.
+ * This file gets replaced during assessment with the original content that
+ * we have for assessment. If you write your code in this file, you will end up deleting it in the assessment.
+ * Uncomment the below lines to do local assessment
+ */
+magazines = [
+  
+  "https://flipboard.com/@wpreview/modi-s-hindu-nationalist-agenda-is-corroding-india-s-democracy-oa78juike4hrjat7.rss",
+  "https://flipboard.com/topic/indianstartups.rss",
+  "https://flipboard.com/topic/aajtak.rss"
+];
